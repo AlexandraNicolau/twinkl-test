aefe6f78fa77a37145d5eca54b7c1745f141655b
Alexandra Nicolau <alexandra.tnicolau@gmail.com>
2025-03-02 18:02:24 +0000

    chore: added tests for getUserbyId

--- src/controllers/user.controller.test.ts
+++ src/controllers/user.controller.test.ts
@@ -1,3 +1,5 @@
+import { UserType } from '../user/user';
+import { addUser } from '../services/user.service';
 import { getUserById } from './user.controller';
 import httpMocks from 'node-mocks-http';
 
@@ -19,5 +21,37 @@ describe('getUserById', () => {
 
     expect(data).toStrictEqual({ error: 'User not found' });
     expect(response.statusCode).toBe(404);
+  });
+
+  it('should return a 200 status code with the user', async () => {
+    const mockUser = {
+      firstName: 'John',
+      lastName: 'Williams',
+      passowrd: 'randomPass123',
+      emailAddress: 'hello@testy.mcTest',
+      createdAt: '2025-03-01T20:46:43.085Z',
+      userType: UserType.student,
+    };
+
+    const newUser = await addUser(mockUser);
+
+    const { id } = newUser;
+
+    const request = httpMocks.createRequest({
+      method: 'POST',
+      url: `/user/${id}`,
+      params: {
+        id,
+      },
+    });
+
+    const response = httpMocks.createResponse();
+
+    await getUserById(request, response);
+
+    const data = JSON.parse(response._getData());
+
+    expect(data).toStrictEqual(newUser);
+    expect(response.statusCode).toBe(200);
   });
 });
